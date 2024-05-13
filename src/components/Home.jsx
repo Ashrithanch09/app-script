c6779b1c85fb02180e8c2e49418874b050080631
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-05-13 18:32:49 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Navbar from "../pages/Navbar";
+import Products from "./Products";
+import "../styles/Home.css";
+import Footer from "../pages/Footer";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <Products />
+      <Footer />
+    </div>
+  );
+};
 
+export default Home;
